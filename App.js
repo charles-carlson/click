83b656c7ad2312b7a06dda6d2f424cfab057df00
Will Roberts <robert9@stolaf.edu>
2020-05-07 19:16:06 -0500

    added NavigationContainer

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+// tutorial on installing NavigationContainer:
+// https://reactnative.dev/docs/navigation
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
+    </NavigationContainer>
   );
 }
 
