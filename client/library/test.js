d4bdd2476352ec57a33d41f478316baf6afd55f0
Charles Carlson <carlson.charles20@gmail.com>
2020-05-11 21:36:33 -0500

    added saved history points to user

--- client/library/test.js
+++ client/library/test.js
@@ -11,7 +11,18 @@ export default class MainScreen extends Component {
         this.handlePress = this.handlePress.bind(this);
         this.logout = this.logout.bind(this)
     }
-
+    componentDidMount(){
+        fetch('http://192.168.0.12:3001/score/getScore').then(res=>{
+            return res.json()
+            .then(myjson=>{
+                console.log(myjson.rows[0].points)
+                this.setState({score:myjson.rows[0].points})
+            })
+        }).catch(err=>{
+            console.log(err)
+            throw err;
+        })
+    }
     logout(e){
         fetch('http://192.168.0.12:3001/logout').then(res=>{
             Alert.alert('Logged out')
